f31c97ca7af2bd9f44bd976de795dc9f841c9605
Chris Beldam <chrisgbeldam@gmail.com>
2019-01-29 18:44:06 +0000

    Starting User Login/Logout

--- workio_project/urls.py
+++ workio_project/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('contract/<int:pk>/delete/', views.ContractDeleteView.as_view(), name='contract-delete'),
     path('contract/<int:pk>/', views.ContractDetailView.as_view(), name='contract-detail'),
     path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name = "users/login.html"), name="login",),
+    path('logout/', auth_views.LogoutView.as_view(template_name = "users/logout.html"), name="logout"),
 ]
